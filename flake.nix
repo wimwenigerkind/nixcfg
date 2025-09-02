09f209382ce875719f61c288b933a6491ee0196f
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-02 20:27:06 +0200

    Add direnv

--- flake.nix
+++ flake.nix
@@ -179,6 +179,10 @@
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
+      programs = {
+        direnv.enable = true;
+      };
+
       # Enable alternative shell support in nix-darwin.
       # programs.fish.enable = true;
 
